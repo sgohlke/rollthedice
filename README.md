14ab2aee191c849dacc59354d7db88a12ecf6e0b
stefangohlke <gohlke.stefan@yahoo.de>
2022-02-26 00:09:35 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rollthedice
 Short dice game for introduction to programming
+
+# About this
+This is used as an example for a introduction to coding session. You can run it on the [Github pages](https://sgohlke.github.io/rollthedice/) page of this project.
---
+++ index.js
@@ -0,0 +1,42 @@
+const DEFAULT_NUMBER_OF_ROUNDS = 10;
+const highestNumber = 6;
+
+function generateRandomNumber() {
+  const randomNumber = Math.random() * highestNumber
+  //console.log('Random number is', randomNumber);
+  return Math.ceil(randomNumber);
+}
+
+function markMinMaxNumber(currentNumber) {
+  if (currentNumber === 1) {
+    return '#'
+  } else if (currentNumber === 6) {
+    return '*'
+  } else return ''
+}
+
+function getNumberOfRounds() {
+  const rounds = document.getElementById("numberOfRounds");
+  if (rounds && rounds.value) {
+    return Number(rounds.value);
+  } else return DEFAULT_NUMBER_OF_ROUNDS;
+}
+
+
+function getResults() {
+  const numberOfRoundsToPlay = getNumberOfRounds();
+
+  console.log(new Date().toISOString(), 'Rolling the dice');
+  let resultNumbers = '';
+  let sum = 0;
+  let currentNumber = 0;
+  for (let counter = 0; counter < numberOfRoundsToPlay; counter++) {
+    currentNumber = generateRandomNumber();
+    resultNumbers += currentNumber + markMinMaxNumber(currentNumber) + (counter < numberOfRoundsToPlay - 1 ? '---' : '')
+    sum += currentNumber
+  }
+
+  document.getElementById('result').innerHTML = 'Die gewürfelten Zahlen sind <br>' + resultNumbers + '<br>Die Summe der Zahlen ist: ' + sum;
+  console.log(new Date().toISOString(), 'Finished Rolling the dice');
+
+}
